1907f8fee8f037fa30cc947540dc7f25f1585f8c
laurenmstuart <laurenmstuart@gmail.com>
2016-09-11 17:39:34 -0400

    initial commit
    Foundational commit, containing all files and updated README

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # cerias-sa_cipherlab
+## Synopsis
+
+Beginner and intermediate lab for substitution ciphers, in Python, with virtual machine image for uniform lab environment. 
+
+## Motivation
+
+This lab was created for CERIAS-SA (CERIAS Student Association) members with an interest in cryptography but little to no background. The event brought students in to meet others, work together, and starter skills in an information security topic.
+
+## Contents
+
+ - Event page, for posterity
+ - CipherLab slides, the accompanying lecture for the lab
+ - Handout, with highlights from the lecture for reference
+ - caesar-lab.py, a source file for lab attendees to fill in code, with comment prompts
+ - vigenere-lab.py, a source file for lab attendees to fill in code, with comment prompts
+ - challenge_ciphertext_caesar.txt, a text file containing a caesar ciphertext, for intermediate students to attempt to decipher
+ - challenge_ciphertext_vigenere.txt, a text file containing a vigenere ciphertext, for intermediate students to decipher
+ - (solution) challenge_plaintext.txt, the associated plaintext 
+ - (solution) caesar-solution.py, a source file illustrating one way of solving the caesar-lab
+ - (solution) vigenere-solution.py, a source file illustrating one way of solving the vigenere-lab
+ 
+ NOT included: 
+ - The virtual machine referred to in the slides and event page. 
+ - Solution files. 
+
+## Python version: 3.5.2
+ 
+## Contributors
+
+This project is not under active development. 
+
+## Credits
+
+The source files and instructional materials were authored by Lauren M. Stuart. The event flyer, appearing in the first slide, was created by Mai Seto. The virtual machine image was created by Austin Klasa. Thanks also to Dan Sokoler and Wes LaFleur for help in putting on the event. 
+
+## License
+
+A short snippet describing the license (MIT, Apache, etc.)
---
+++ vigenere-lab.py
@@ -0,0 +1,185 @@
+#!/usr/bin/python
+
+##file: vigenere-solution.py
+##author: Lauren M. Stuart laurenmstuart@gmail.com
+##created for CERIAS Student Association activity CipherLab
+## -- feb 11, 2016, please email with questions!
+
+##    Copyright (C) 2016 Lauren M. Stuart
+##
+##    This program is free software: you can redistribute it and/or modify
+##    it under the terms of the GNU General Public License as published by
+##    the Free Software Foundation, either version 3 of the License, or
+##    (at your option) any later version.
+##
+##    This program is distributed in the hope that it will be useful,
+##    but WITHOUT ANY WARRANTY; without even the implied warranty of
+##    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##    GNU General Public License for more details.
+##
+##    You should have received a copy of the GNU General Public License
+##    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import random
+
+_alphabet = 'abcdefghijklmnopqrstuvwxyz'
+## we'll use _alphabet.find() to find the index
+##(and therefore integer value) of a letter
+## the underscore is a personal naming convention for global variables
+
+_mod = 26
+## the length of our alphabet, and our modulus
+
+_mode = 'E'
+#_mode = 'D'
+##set our mode to 'E'ncrypt or 'D'ecrypt; comment out the line that should not be used
+
+_message = 'CIPHERSSUREAREFUN'
+##to start, make the message without spaces or punctuation
+## --> gain a level: change the code to allow for spaces and [some] punctuation in the message
+## --> gain a level: change the code to strip all non-alphabetical characters from an arbitrary message
+
+_ciphertext = ''
+## to start, make an empty ciphertext variable;
+##  copy and paste the output of the encryption when it's done
+
+
+_key = 'cerias'
+##to start, make the key without spaces or punctuation
+## --> gain a level: change the code to allow for spaces and [some] punctuation in the key
+##     (this may be easier after you have done the same for the message)
+
+
+##--> gain a level: change the code to accept arbitrary text for key via console input
+##--> gain a level: change the code to accept text from a written file
+
+def main():
+    
+    ## get the key length. use len(str) on strings, .length() on lists
+
+    if _mode == 'E':
+        encrypt_message(_key, _message)
+
+    elif _mode == 'D':
+        decrypt_message(_key, _ciphertext)
+
+
+def encrypt_message(key, message):
+
+    keylength = len(key)
+    
+    keycursor = 0
+    ##set our key pointer to the first character
+    ## you can use a different way to keep track of where in the key we should be; this is a suggestion.
+    
+    ciphertext = ''
+    ##start a new empty string to put encrypted letters into
+    
+
+    for letter in message:
+        letter = letter.lower()
+        ##change the letter to lowercase for simplicity (in case it is not)
+
+        
+        ##get the integer value of the letter in the message
+
+
+        ##get the letter at the cursor's current position in the key
+
+
+        ##change the letter to lowercase for simplicity (in case it is not)
+
+
+        ##get the integer value of the letter in the key
+
+
+        if ### != -1 and ### != -1: ##(replace '###' with your variable names)
+        ##make sure both the values are in range
+        ## This is a good practice for secure programming!
+        ## gain a level: why is this a good practice for secure programming?
+
+            
+            ##add the two integer values together, and "wrap around" by using the modulus
+
+         
+            ##get the letter with the integer value of Cvalue
+
+
+            ##add the new letter to our building ciphertext string
+            ## use += for strings, and .append() for lists
+
+
+            ##update our cursor (remember to wrap around the key as well)
+
+        else:
+            print("you've got a non-alphabetic character")
+
+    print(ciphertext)
+    ##print the new ciphertext at the end
+
+
+
+
+def decrypt_message(key, ciphertext):
+
+    keylength = len(key)
+    
+    keycursor = 0
+    ##set our key pointer to the first character
+    
+    plaintext = ''
+    ##start a new empty string to put encrypted letters into
+    
+
+    for letter in ciphertext:
+        letter = letter.lower()
+        ##change the letter to lowercase for simplicity (in case it is not)
+
+
+        ##get the integer value of the letter in the message
+
+
+        ##get the letter at the cursor's current position in the key
+
+
+        ##change the letter to lowercase for simplicity (in case it is not)
+
+
+        ##get the integer value of the letter in the key
+
+        if ### != -1 and ### != -1: ##(replace '###' with your variable names)
+        ##make sure both the values are in range
+        ## This is a good practice for secure programming!
+        ## --> gain a level: why is this a good practice for secure programming?
+
+
+            ##subtract the key letter value from the ciphertext letter value,
+            ## and "wrap around" by using the modulus
+            
+            #print("Cvalue " + str(Cvalue) + " Kvalue " + str(Kvalue))
+            ##this is a commented-out print statement used to debug
+
+            
+            ##get the letter with the integer value of Mvalue
+
+
+            ##add the new letter to our building plaintext string
+            ## use += for strings, and .append() for lists
+
+
+            ##update our cursor (remember to wrap around the key as well)
+
+        else:
+            print("you've got a non-alphabetic character")
+
+    print(plaintext)
+    ##print the new plaintext at the end
+    
+
+
+
+##if this file is run on its own, rather than imported, call main()
+if __name__ == '__main__':
+    main()
+
+
